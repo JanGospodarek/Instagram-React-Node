96d8c4c9587e0843528258f9cb1569d79250177e
JanGospodarek <jan.gospo@onet.pl>
2023-04-18 10:50:15 +0200

    additional routers

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const http = require('http');
-const logger = require('tracer').colorConsole();
+const http = require("http");
+const logger = require("tracer").colorConsole();
 
-const router = require("./app/router")
+const router = require("./app/router");
+
+// http
+//     .createServer((req, res) => )
+//     .listen(3000, () => logger.log("listen on 3000"))
 
 http
-    .createServer((req, res) => router(req, res))
-    .listen(3000, () => logger.log("listen on 3000"))
+  .createServer(async (req, res) => {
+    //images
+
+    if (req.url.search("/api/photos") != -1) {
+      await imageRouter(req, res);
+    }
+
+    //tags
+    else if (req.url.search("/api/tags") != -1) {
+      await tagsRouter(req, res);
+    } else {
+      router(req, res);
+    }
+  })
+  .listen(PORT, () => console.log("listen on 3000"));
